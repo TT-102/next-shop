5ff5b496f09e61bbfd41f5d250d7cfcc49174bd3
Tho Tran <tho@lumit.app>
2024-11-03 11:02:08 +0100

    fix: navigation bar

---
+++ app/components/navigation/MobileNav.tsx
@@ -0,0 +1,41 @@
+import { ImCross } from "react-icons/im";
+import Link from "next/link";
+
+interface Props {
+  showNav: boolean;
+  closeNav: () => void;
+}
+
+export default function MobileNav({ showNav, closeNav }: Props) {
+  const navStyle = showNav ? "translate-x-0" : "translate-x-[-100%]";
+  return (
+    <div
+      className={`${navStyle} fixed right-0 transition-all duration-500 left-0 top-0 bottom-0 h-[100vh] 
+    bg-[#000000e0] z-[1002]`}
+    >
+      <ImCross
+        onClick={closeNav}
+        className="absolute top-[2rem] right-[2rem] w-[2rem] h-[2rem] text-white"
+      />
+      <div
+        className={`${navStyle} bg-emerald-700 transition-all duration-500 delay-200 flex flex-col items-center
+      justify-center w-[78%] h-[100%]`}
+      >
+        <ul className="space-y-10">
+          <li className="text-[18px] font-medium tracking-wide text-white">
+            <Link href="/">HEM</Link>
+          </li>
+          <li className="text-[18px] font-medium tracking-wide text-white">
+            <Link href="/">BUTIK</Link>
+          </li>
+          <li className="text-[18px] font-medium tracking-wide text-white">
+            <Link href="/">MENY</Link>
+          </li>
+          <li className="text-[18px] font-medium tracking-wide text-white">
+            <Link href="/">KONTAKT</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
