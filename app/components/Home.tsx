9001737cf4abe17a4cee9809402dc603a2a45518
Tho Tran <tho@lumit.app>
2024-11-03 12:47:11 +0100

    feat: create information section, styling

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,9 +1,11 @@
 import MainSlider from "./slider/MainSlider";
+import Feature from "./features/Feature";
 
 export default function Home() {
   return (
     <div className="overflow-hidden">
       <MainSlider />
+      <Feature />
     </div>
   );
 }
---
+++ app/components/features/Feature.tsx
@@ -0,0 +1,56 @@
+import Img1 from "@/public/images/f1.jpg";
+import Img2 from "@/public/images/f2.jpg";
+import Img3 from "@/public/images/f3.jpg";
+import Image, { StaticImageData } from "next/image";
+
+interface BurgerCardProps {
+  imgSrc: StaticImageData | string;
+  title: string;
+  description: string;
+  additionalClass?: string;
+}
+
+const BurgerCard: React.FC<BurgerCardProps> = ({
+  imgSrc,
+  title,
+  description,
+  additionalClass,
+}) => (
+  <div
+    className={`p-6 hover:bg-white rounded-lg hover:rounded-3xl transition-all duration-200 ${additionalClass}`}
+  >
+    <Image src={imgSrc} alt={title} className="rounded-3xl" />
+    <h3 className="font-semibold text-[20px] mt-[20px]">{title}</h3>
+    <p className="mt-[0.2rem]">{description}</p>
+  </div>
+);
+
+export default function Feature() {
+  return (
+    <div className="pt-[5rem] pb-[3rem]">
+      <h1 className="heading">Hamburgare tillagad med kärlek</h1>
+
+      <div
+        className="w-[100%] lg:w-[80%] mt-[3rem] md:mt-[5rem] mb-[3rem] mx-auto grid grid-cols-1
+      md:grid-cols-2 lg:grid-cols-3 gap-{3rem}"
+      >
+        <BurgerCard
+          imgSrc={Img1}
+          title="Smashed burger"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti veniam ducimus tempora deserunt magni! Temporibus, odio molestiae commodi quam nisi aperiam repellendus. Porro unde deserunt veritatis, totam accusamus vitae sit!"
+        />
+        <BurgerCard
+          imgSrc={Img2}
+          title="Hamburage måltid"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti veniam ducimus tempora deserunt magni! Temporibus, odio molestiae commodi quam nisi aperiam repellendus. Porro unde deserunt veritatis, totam accusamus vitae sit!"
+          additionalClass="lg:translate-y-[3rem]"
+        />
+        <BurgerCard
+          imgSrc={Img3}
+          title="Lorem ipsum"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti veniam ducimus tempora deserunt magni! Temporibus, odio molestiae commodi quam nisi aperiam repellendus. Porro unde deserunt veritatis, totam accusamus vitae sit!"
+        />
+      </div>
+    </div>
+  );
+}
