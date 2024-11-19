a7b6b672ab4e0e8ea319a6c65dc35b5e73e90faf
Tho Tran <tr.anhtho@gmail.com>
2024-11-19 21:24:49 +0100

    style: about page: add image, backgound & text styling

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,25 @@
 import styles from "./page.module.css";
+import Image from "next/image";
+import BurgerIgm2 from "@/public/images/b2.png";
 
 export default function AboutPage() {
   return (
     <div>
-      <h2 className={styles.aboutHeader}>OM OSS</h2>
+      <div className={styles.aboutpageWrapper}>
+        <div className={styles.imgWrapper}>
+          {/* <div className={styles.textBox}>
+            <p className={styles.aboutHeader}>vår passion</p>
+            <p className={styles.aboutHeader}>vårt liv</p>
+            <p className={styles.aboutHeader}>vår framtid</p>
+          </div> */}
+          <div className={styles.verticalText}>OM OSS</div>
+          <Image
+            src={BurgerIgm2}
+            alt="Hamburger about page"
+            className={styles.burgerImg}
+          />
+        </div>
+      </div>
     </div>
   );
 }
