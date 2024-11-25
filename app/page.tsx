c8933997e3ce10970683b8640a3bb4088a349670
Tho Tran <tr.anhtho@gmail.com>
2024-11-25 20:57:10 +0100

    fix: issue fetch api appear in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import AboutPage from "./about/page";
 import Home from "./components/Home";
 
 export default function () {
   return (
     <div className="bg-[#f4f1ea]">
       <Home />
-      <AboutPage />
     </div>
   );
 }
